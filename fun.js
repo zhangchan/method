7b81e33845a445be5884c2984cfca1ce98360ace
EastC <1908530584@qq.com>
2016-07-21 17:35:04 +0800

    Add files via upload

--- fun.js
+++ fun.js
@@ -1,24 +1,136 @@
 /***
 ToDoList:
 浏览器检测
-日期格式化
+DateFormat
 组织冒泡
-判断Class是否存在，增删class，getclass
-事件绑定，移除
+判断Class是否存在，增删class，
+事件移除 
 自定义鼠标右键菜单
 拖拽
-
-
+ 
+ 
 DoneList:
-计算中文长度
-字符串转日期
-**/
+calCn
+toDate
+getClass 
+getCharCode
+removeEvent
+loadScript
+arrSort
+**/ 
 
 /***
-method：计算中文长度
-time：2016/03/23
-devBy：ZhangChan
+method：arrSort
+time：2016/05/10
+devBy：EastC
+desc: ["1-2","1-1","2-1" ... "1-2-2",1,2,3] 排序
+***/
+var arrs = ["1-2","1-1","2-1","2-2","1-1-1","1-1-2","1-2-1","1-2-2",1,2,3];
+// [1,2,3,"1-1","1-2","2-1","2-2","1-1-1","1-1-2","1-2-1","1-2-2"]
+function arrSort(arrs){
+    var subArr = [];
+    var newArr = arrs; 
+    var lastArr = [];
+    for(var i = 0; i < newArr.length; i++){
+        newArr[i] = newArr[i].toString().split("-");
+        if(!subArr[newArr[i].length]){
+            subArr[newArr[i].length] = [];
+        }
+        subArr[newArr[i].length].push(arrs[i]);
+        subArr[newArr[i].length].sort();
+    }
+    for(var key in subArr){
+        for(var i=0; i < subArr[key].length;i++){
+            subArr[key][i] = subArr[key][i].join("-");
+        }
+        lastArr = lastArr.concat(subArr[key]);
+    }
+    return lastArr;
+}
+
+/***
+method：loadScript
+time：2016/05/10
+devBy：EastC
+desc: 动态载入js
+***/
+function loadScript(url){
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+/***
+method：getCharCode
+time：2016/04/11
+devBy：EastC
 desc:
+**/
+function getCharCode(evt){
+	var e = evt || window.event;
+	if(typeof e.charCode == "number"){
+		return e.charCode;
+	}else{
+		return e.keyCode;
+	}
+}
+
+/***
+method：removeEvent
+time：2016/04/11
+devBy：EastC
+desc: 事件移除
+**/
+
+function removeEvent(obj, type, fn){
+	if(typeof obj.removeEventListener !="undefined"){
+		obj.removeEventListener(type, fn, false);
+	}else if(typeof obj.detachEvent !="undefined"){
+		obj.detachEvent("on" + type,fn);
+	}
+}
+
+/***
+method：事件绑定
+time：2016/04/01
+devBy：EastC
+desc:
+**/
+
+function addEvent(obj, type, fn){
+	if(typeof obj.addEventListener !="undefined"){
+		obj.addEventListener(type, fn, false);
+	}else if(typeof obj.attachEvent !="undefined"){
+		obj.attachEvent("on" + type,function(){
+			fn.call(obj);
+		})
+	}
+}
+
+/***
+method：getClass
+time：2016/04/01
+devBy：EastC
+desc:
+**/
+
+var getClass = function (className){
+	var all = document.getElementsByTagName("*");
+	var ele= [];
+	for(var i=0; i<all.length; i++){
+		if(all[i].className == className){
+			ele.push(all[i]);
+		}
+	}
+	return ele;
+}
+
+/***
+method：calCn
+time：2016/03/23
+devBy：EastC
+desc: 计算中文长度
 **/
 function calCn(str) {
 	re = /[\u4E00-\u9FA5]/g;
@@ -30,20 +142,20 @@ function calCn(str) {
 }
 
 /***
-method：字符串转日期
+method：toDate
 time：2016/03/23
-devBy：ZhangChan
-desc:
+devBy：EastC
+desc: 字符串转日期
 **/
 function toDate(date) {
 	return new Date(date);
 }
 
 /***
-method：日期格式化
+method：DateFormat
 time：2016/03/23
-devBy：ZhangChan
-desc:
+devBy：EastC
+desc: 日期格式化
 **/
 function DateFormat(date, pattern ){
 
@@ -52,7 +164,7 @@ function DateFormat(date, pattern ){
 /**
 method：浏览器检测
 time：2016/03/23
-devBy：ZhangChan
+devBy：EastC
 desc:
 **/
 (function(){
@@ -66,4 +178,4 @@ desc:
 	
 	// console.log(ua,(/chrome\/([\d.]+)/).test(ua),s,sys.chrome);
 })();
-alert(sys.firefox);
+// alert(sys.firefox);
